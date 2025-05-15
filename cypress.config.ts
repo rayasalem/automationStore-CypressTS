40ec67d2c4e6e1c46ee7f728b69bc8fddbf7e0fe
Fadi Aburub <faburub@pma.ps>
2025-05-15 13:49:22 +0300

    Add final Cypress setup, ESLint, Prettier config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
-    baseUrl: 'https://automationteststore.com/'
+    baseUrl: 'https://automationteststore.com/', 
+    supportFile: "cypress/support/e2e.ts"
   },
 });
