d6617fff1810b49244541c3fee737065b1d86e1d
rayasalem <rayasalem483@gmail.com>
2025-07-02 00:35:11 +0300

    save all  changes

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'dtvw54',
   e2e: {
     setupNodeEvents(on, config) {
+      // استدعاء require داخل الدالة
       const allureWriter = require('@shelex/cypress-allure-plugin/writer');
       allureWriter(on, config);
       return config;
     },
     baseUrl: 'https://automationteststore.com/',
-    supportFile: "cypress/support/e2e.ts"
+    supportFile: "cypress/support/e2e.ts",
   },
 });
