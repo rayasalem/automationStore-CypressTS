d2e650a3f2bf11ad5bef2424bf26be83a016c100
rayasalem <rayasalem483@gmail.com>
2025-07-05 01:49:42 +0300

    add check cart  price file  and esome edit at cart page < after that  i test my code and all  test  is sucessfull

--- automationStore-CypressTS-rayasalemLogin/cypress/e2e/spec-copy-1.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Login Test on OrangeHRM', () => {
-  it('logs in successfully', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/');
-    cy.get('input[name="username"]').type('Admin');
-    cy.get('input[name="password"]').type('admin123');
-    cy.get('button[type="submit"]').click();
-    cy.url().should('include', '/dashboard');
-  });
-});
