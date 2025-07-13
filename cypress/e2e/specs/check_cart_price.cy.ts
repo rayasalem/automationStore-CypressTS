f058f4d6fd6fef104bf9754e3bf40d4ae57ffd97
Rayaalislam Salem <120057726+rayasalem@users.noreply.github.com>
2025-07-13 15:44:05 +0300

    Update check_cart_price.cy.ts

--- cypress/e2e/specs/check_cart_price.cy.ts
+++ cypress/e2e/specs/check_cart_price.cy.ts
@@ -3,15 +3,13 @@ import CartPage from "cypress/support/pages/cart_page";
 describe("Cart Price Check Test Suite", () => {
   it("user adds first product to cart and checks prices and totals", () => {
     cy.visit('/');
-    cy.get('.prdocutname').first().click();
-    cy.url().should('include', 'product');
-    cy.get('a.cart').click();
-    cy.wait(3000);
     const cart = new CartPage();
-    cart.visitCart();
-    cart.assertProductsExist();
-    cart.assertEachProductPrice();
-    cart.assertSubtotalPerRow();
-    cart.assertTotalCart();
+
+    cart.add_first_product_to_cart();
+    cart.visit_cart();
+    cart.assert_products_exist();
+    cart.assert_each_product_price();
+    cart.assert_subtotal_per_row();
+    cart.assert_total_cart();
   });
 });
